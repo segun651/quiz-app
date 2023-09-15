886c9e5d21f0acc5afa0172fe5dba954551297d1
Segun <omojolasegun222@gmail.com>
2023-09-15 04:01:24 -0700

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,535 @@
+let questions = [
+       {
+              question: " Who won the ballon d'or in the year 2018.",
+              option1: "A. Cristiano Ronaldo",
+              option2: "B. Lionel Messi",
+              option3: "C. Luka Modric",
+              option4: "D. Eden Hazard",
+              answer: "C. Luka Modric"
+       },
+       {
+              question: " Who won the golden boot in the 2018 world cup.",
+              option1: "A. Cristiano Ronaldo",
+              option2: "B. Lionel Messi",
+              option3: "C. Luka Modric",
+              option4: "D.Harry Kane",
+              answer: "D.Harry Kane"
+       },
+       {
+              question: " Who won the golden boot in the 2022 world cup.",
+              option1: "A.Kylian Mbappe",
+              option2: "B. Lionel Messi",
+              option3: "C. Neymar Jr",
+              option4: "D.Harry Kane",
+              answer: "A.Kylian Mbappe"
+       },
+
+       {
+              question: " Who won the golden boot in 2010 world cup.",
+              option1: "A. Diego Forlan",
+              option2: "B. Lionel Messi",
+              option3: "C. Andres Iniesta",
+              option4: "D. Xavi Hernandez",
+              answer: "A. Diego Forlan"
+       },
+       {
+              question: " Which team  has the highest number of champions league.",
+              option1: "A. Arsenal",
+              option2: "B. Man utd",
+              option3: "C. Liverpool",
+              option4: "D. Real Madrid",
+              answer: "D. Real Madrid"
+       },
+       {
+              question: " Which team  has the highest number of Fa Cups.",
+              option1: "A. Arsenal",
+              option2: "B. Man utd",
+              option3: "C. Liverpool",
+              option4: "D.Man City",
+              answer: "A. Arsenal"
+       },
+       {
+              question: " Which team won the 1999 champions league",
+              option1: "A. Barcelona",
+              option2: "B. Arsenal",
+              option3: "C. Real Madrid",
+              option4: "D. Man utd",
+              answer: "D. Man utd"
+       },
+       {
+              question: " Which team homeground is called Camp Nou",
+              option1: "A. Barcelona",
+              option2: "B. Arsenal",
+              option3: "C. Real Madrid",
+              option4: "D. Man utd",
+              answer: "A. Barcelona"
+       },
+       {
+              question: " Which footballer has the record of most headed goals",
+              option1: "A. Cristiano Ronaldo",
+              option2: "B. Lionel Messi",
+              option3: "C. Pedro",
+              option4: "D. Eden Hazard",
+              answer: "A. Cristiano Ronaldo"
+       },
+       {
+              question: " Which footballer has the record of the most free kicks goal",
+              option1: "A. Cristiano Ronaldo",
+              option2: "B. Lionel Messi",
+              option3: "C.Juninho",
+              option4: "D. Ronaldinho",
+              answer: "C.Juninho"
+       },
+       {
+              question: " Who scored the only goal in the 2014 world cup final.",
+              option1: "A. Cristiano Ronaldo",
+              option2: "B. Mario Gotze",
+              option3: "C. Mascherano",
+              option4: "D. Thomas Muller",
+              answer: "B. Mario Gotze"
+       },
+       {
+              question: " Who scored the second goal in the 2019 Champions league final.",
+              option1: "A. Cristiano Ronaldo",
+              option2: "B. Origi",
+              option3: "C. Mo Salah",
+              option4: "D. Thomas Muller",
+              answer: "B. Origi"
+       },
+       {
+              question: " Which player scored the fastest hat-trick in the Premier League.",
+              option1: "A. Cristiano Ronaldo",
+              option2: "B. Sadio Mane",
+              option3: "C. Luka Modric",
+              option4: "D. Erling Haaland",
+              answer: "B. Sadio Mane"
+       },
+       {
+              question: " Which team won the first Premier League title.",
+              option1: "A. Manchester United",
+              option2: "B.Arsenal",
+              option3: "C. Chelsea",
+              option4: "D.Liverpool",
+              answer: "A. Manchester United"
+
+       },
+       {
+              question: " With 202 clean sheets, which goalkeeper has the best record in the Premier League.",
+              option1: "A. David De Gea",
+              option2: "B. Alison Becker",
+              option3: "C. Petr Cech",
+              option4: "D.Peter Schemchiel",
+              answer: "C. Petr Cech"
+       },
+       {
+              question: "Which country won the first ever World Cup in 1930.",
+              option1: "A. Brazil",
+              option2: "B. Argentina",
+              option3: "C.France",
+              option4: "D.Uruguay",
+              answer: "D.Uruguay"
+       },
+       {
+              question: "Which club is associated with 'Galacticos'.",
+              option1: "A. Brazil",
+              option2: "B.Real Madrid",
+              option3: "C. Barcelona",
+              option4: "D.AC Milan",
+              answer: "B.Real Madrid"
+       },
+       {
+              question: " With 365 goals, who holds the record for top Bundesliga goalscorer of all time.",
+              option1: "A. Marco Reus",
+              option2: "B. Robert Lewandowski",
+              option3: "C. Gerd Muller",
+              option4: "D.Thomas Muller",
+              answer: "C. Gerd Muller"
+       },
+       {
+              question: " Who is the only player to win the Champions League with three different clubs.",
+              option1: "A. Ronaldinho",
+              option2: "B. Steven Gerrad",
+              option3: "C. Cristiano Ronaldo",
+              option4: "D.Clarence Seedorf",
+              answer: "D.Clarence Seedorf"
+       },
+       {
+              question: "Which player holds the record for most Champions League winners' medals.",
+              option1: "A. Francisco Gento",
+              option2: "B.Cristiano Ronaldo",
+              option3: "C. Lionel Messsi",
+              option4: "D.Zinedine Zidane",
+              answer: "A. Francisco Gento"
+       },
+       {
+              question: "Ronaldo helped Portugal win the European Championship in which year.",
+              option1: "A.2014",
+              option2: "B. 2012",
+              option3: "C. 2016",
+              option4: "D.2009",
+              answer: "C. 2016"
+       },
+       {
+              question: "Which manager was famously said to have given players 'the Hairdryer Treatment'",
+              option1: "A.Sir Alex Ferguson",
+              option2: "B. Sir Bobby Charlton",
+              option3: "C. Pep Guardiola",
+              option4: "D.Carlo Ancelloti",
+              answer: "A.Sir Alex Ferguson"
+       },
+       {
+              question: "Which country  won the 2006 world cup.",
+              option1: "A.France",
+              option2: "B.Argentina",
+              option3: "C. Italy",
+              option4: "D.Spain",
+              answer: "C. Italy"
+       },
+       {
+              question: "Which is the most decorated Scottish Manager in the Premier League",
+              option1: "A.Sir Alex Ferguson",
+              option2: "B. Sir Bobby Charlton",
+              option3: "C.Mark Hughes",
+              option4: "D.Rafael Benitez",
+              answer: "A.Sir Alex Ferguson"
+       },
+       {
+              question: "Which club is sometimes referred to as FC Hollywood",
+              option1: "A.Arsenal",
+              option2: "B.Al Hilal",
+              option3: "C. Bayern Munich",
+              option4: "D.Liverpool",
+              answer: "C. Bayern Munich"
+       },
+       {
+              question: "RB Leipzig are otherwise known as",
+              option1: "A.The Matadors",
+              option2: "B.The Red Bulls",
+              option3: "C. Galacticos",
+              option4: "D.The Hammers",
+              answer: "B.The Red Bulls"
+       },
+       {
+              question: "How many times has Germany won the Fifa World Cup",
+              option1: "A.Zero",
+              option2: "B.One",
+              option3: "C. Five",
+              option4: "D.Four",
+              answer: "D.Four"
+       },
+       {
+              question: "How many times has Manchester united won the champions league",
+              option1: "A.Zero",
+              option2: "B.One",
+              option3: "C. Five",
+              option4: "D.Three",
+              answer: "D.Three"
+       },
+       {
+              question: "What is the home of Manchester united also called. ",
+              option1: "A.Theatre Of Dreams",
+              option2: "B.Old Town Road",
+              option3: "C. Stamford Bridge",
+              option4: "D.San Siro",
+              answer: "A.Theatre Of Dreams"
+       },
+       {
+              question: " Who won the ballon d'or in the year 2008.",
+              option1: "A. Cristiano Ronaldo",
+              option2: "B. Lionel Messi",
+              option3: "C. Ronaldinho",
+              option4: "D. Wayne Rooney",
+              answer: "A. Cristiano Ronaldo"
+       },
+       {
+              question: "Which team won the 2010 champions league",
+              option1: "A.Arsenal",
+              option2: "B.Barcelona",
+              option3: "C. Inter Milan",
+              option4: "D.Liverpool",
+              answer: "C. Inter Milan"
+       },
+       {
+              question: "What year was the Premier League founded.",
+              option1: "A.1992",
+              option2: "B.1863",
+              option3: "C. 1990",
+              option4: "D.1608",
+              answer: "A.1992"
+       },
+       {
+              question: "Which club  as won the most Premier league titles.",
+              option1: "A.Arsenal",
+              option2: "B.Chelsea",
+              option3: "C.Manchester United",
+              option4: "D.Liverpool",
+              answer: "C.Manchester United"
+       },
+      
+       {
+              question: "Who is the Premier League all time goal scorer",
+              option1: "A.Harry Kane",
+              option2: "B.Alan Shearer",
+              option3: "C. Mo Salah",
+              option4: "D.Michael Owen",
+              answer: "B.Alan Shearer"
+       },
+       {
+              question: "How many champions league title does Ac Milan have",
+              option1: "A.Five",
+              option2: "B.Nine",
+              option3: "C. Seven",
+              option4: "D.Two",
+              answer: "C. Seven"
+       },
+       {
+              question: "Which club is sometimes referred to as The Hammers",
+              option1: "A.Arsenal",
+              option2: "B.West Ham",
+              option3: "C. Tottenham Hotspur",
+              option4: "D.Liverpool",
+              answer: "B.West Ham"
+       },
+       {
+              question: "Where was the 2010 world cup hosted",
+              option1: "A.South Africa",
+              option2: "B.Spain",
+              option3: "C. Italy",
+              option4: "D.Brazil",
+              answer: "A.South Africa"
+       },
+       {
+              question: "What is football club Arsenal also referred as",
+              option1: "A.Charity",
+              option2: "B.Vikings",
+              option3: "C. The Gunners",
+              option4: "D.Seagulls",
+              answer: "C. The Gunners"
+       },
+       {
+              question: "Who won the 2010 world cup ",
+              option1: "A.South Africa",
+              option2: "B.Spain",
+              option3: "C. Germany",
+              option4: "D.Brazil",
+              answer: "B.Spain"
+       },
+          
+       {
+              question: "Who is the only player to score in five different world cups",
+              option1: "A. Cristiano Ronaldo",
+              option2: "B. Lionel Messi",
+              option3: "C. Samuel Eto",
+              option4: "D. Eden Hazard",
+              answer: "A. Cristiano Ronaldo"
+       },
+       {
+              question: "How many ballon d'or does Zinedine Zidane have",
+              option1: "A.Zero",
+              option2: "B.One",
+              option3: "C. Five",
+              option4: "D.Three",
+              answer: "B.One"
+       },
+       {
+              question: "Who is the all time Laliga goalscorer",
+              option1: "A. Cristiano Ronaldo",
+              option2: "B. Lionel Messi",
+              option3: "C. David Villa",
+              option4: "D. Eden Hazard",
+              answer: "B. Lionel Messi"
+       },
+       {
+              question: " Who scored the only goal in the 2010 world cup final.",
+              option1: "A. Cristiano Ronaldo",
+              option2: "B. Mario Gotze",
+              option3: "C. Wesley Sneijer",
+              option4: "D. Andres Iniesta",
+              answer: "D. Andres Iniesta"
+       },
+       {
+              question: "Which is a Saudi  Arabian Club",
+              option1: "A.Arsenal",
+              option2: "B.Al Hilal",
+              option3: "C. Bayern Munich",
+              option4: "D.Liverpool",
+              answer: "B.Al Hilal"
+       },
+       {
+              question: "Which team did the invincibles in the Premier League",
+              option1: "A.Arsenal",
+              option2: "B.Al Hilal",
+              option3: "C. Bayern Munich",
+              option4: "D.Liverpool",
+              answer: "A.Arsenal"
+       },
+       {
+              question: "Which team  won the 2020 champions league",
+              option1: "A.Arsenal",
+              option2: "B.Al Hilal",
+              option3: "C. Bayern Munich",
+              option4: "D.Liverpool",
+              answer: "C. Bayern Munich"
+       },
+       {
+              question: "How many times has Arsenal won the champions league",
+              option1: "A.Zero",
+              option2: "B.One",
+              option3: "C. Five",
+              option4: "D.Three",
+              answer: "A.Zero"
+       },
+       {
+              question: "The Old Lady is a nickname for which club",
+              option1: "A.Arsenal",
+              option2: "B.Juventus",
+              option3: "C.Udinese",
+              option4: "D.Liverpool",
+              answer: "B.Juventus"
+       },
+       {
+              question: "How many teams has done the three peat in the Premier League",
+              option1: "A.Two",
+              option2: "B.One",
+              option3: "C. Five",
+              option4: "D.Three",
+              answer: "A.Two"
+       },
+       {
+              question: " Who has won three world cups in the options below.",
+              option1: "A.Andres Iniesta",
+              option2: "B. Lionel Messi",
+              option3: "C. Pele",
+              option4: "D.Maradona",
+              answer: "C. Pele"
+       },
+       {
+              question: " Who scored the famous 'Hand Of God' goal in the 1978 world cup.",
+              option1: "A.Juninho",
+              option2: "B. Franz Beckanbeur",
+              option3: "C. Pele",
+              option4: "D.Maradona",
+              answer: "D.Maradona"
+       },
+
+]
+let scores = 0
+
+const shuffledArray = questions.sort((a, b) => 0.5 - Math.random());
+let soundname = document.getElementById("switch")
+let backaudio = document.querySelector("audio")
+function renderSound() {
+       if (soundname.innerHTML == "Off") {
+              soundname.innerHTML = "On"
+
+              backaudio.autoplay = true
+              backaudio.load()
+              
+
+       }
+       else {
+              soundname.innerHTML = "Off"
+              backaudio.autoplay = false
+              backaudio.load()
+       }
+
+}
+let currentMenu = 0
+let question = document.getElementById("question")
+let Option1 = document.getElementById("Option1")
+let Option2 = document.getElementById("Option2")
+let Option3 = document.getElementById("Option3")
+let Option4 = document.getElementById("Option4")
+let score = document.getElementById("score")
+window.onload = () => {
+
+       Loaded(currentMenu)
+
+       return shuffledArray
+}
+function Loaded() {
+       let quest = questions[currentMenu]
+       question.innerHTML = quest.question
+       Option1.innerHTML = quest.option1
+       Option2.innerHTML = quest.option2
+       Option3.innerHTML = quest.option3
+       Option4.innerHTML = quest.option4
+
+
+}
+let namevalue = document.querySelector(".name")
+let namevalues = document.querySelector(".names")
+let storage = JSON.parse(localStorage.getItem("values"))
+let scorevalues = document.querySelector(".scorevalues")
+let scorevalue = document.querySelector(".scorevalue")
+let win = document.querySelector(".win")
+let lose = document.querySelector(".lose")
+function Win() {
+       win.style.transform = "translateY(0%)"
+       scorevalues.innerHTML = `${scores}`
+       namevalues.innerHTML = `${storage}`
+}
+function Lose() {
+       lose.style.transform = "translateY(-0%)"
+       scorevalue.innerHTML = `${scores}`
+       namevalue.innerHTML = `${storage}`
+}
+
+let modals = document.getElementsByTagName("li")
+for (i = 0; i < modals.length; i++) {
+
+       modals[i].addEventListener("click", filters)
+
+
+}
+
+
+
+function filters(e) {
+       function show() {
+              let mode = document.querySelectorAll("li")
+              for (i = 0; i < mode.length; i++) {
+                     if (mode[i].innerHTML == questions[currentMenu].answer) {
+                            mode[i].style.backgroundColor = "green"
+                     }
+              }
+       }
+
+       if (e.target.innerHTML == questions[currentMenu].answer) {
+
+              green = e.target.style.backgroundColor = "green"
+
+              if (green) {
+                     scores += 5
+                     score.innerHTML = `${scores}`
+                     let sound = document.createElement("audio")
+                     sound.autoplay = true
+                     let source = document.createElement("source")
+                     source.src = "mixkit-classic-click-1117.wav"
+                     sound.appendChild(source)
+              }
+              setTimeout(() => {
+                     currentMenu++
+                     e.target.style.backgroundColor = "black"
+
+                     if (currentMenu > questions.length - 1) {
+                            setTimeout(Win, 1000)
+                     }
+                     Loaded(currentMenu)
+              }, 500)
+
+       }
+       else {
+              let sound = document.createElement("audio")
+              sound.autoplay = true
+              let source = document.createElement("source")
+              source.src = "mixkit-negative-tone-interface-tap-2569.wav"
+              sound.appendChild(source)
+              e.target.style.backgroundColor = "red"
+              setTimeout(show, 150)
+              setTimeout(Lose, 300)
+       }
+}
+
+
+
